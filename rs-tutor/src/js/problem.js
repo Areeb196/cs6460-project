087fab450f1f2aa6fa018b6c4fdaf9899e2abcef
Areeb Ahmed <5704619+Areeb196@users.noreply.github.com>
2024-07-13 18:52:33 -0500

    Problem done

--- rs-tutor/src/js/problem.js
+++ rs-tutor/src/js/problem.js
@@ -2,7 +2,20 @@
 // sessionstorage, localstorage, 
 var evalID;
 var timerID;
-var time_step = 10000;
+var time_step = 0;
+
+// Constants
+const TIME_STEP_LEN = 10000;
+const MILLI_PER_SEC = 1000;
+const START_TIME_LEN = "15:00";
+
+// TD-BKT parameters
+const L = 0.5;
+const S = 0.1;
+const G = 0.1;
+const E_k = 30;
+
+var skill_probs = [L];
 
 function fetch_with_timeout(url, options, timeout = 6000) {
     return Promise.race([
@@ -34,6 +47,9 @@ function evaluateCode() {
         edition: "2021"
     };
 
+    // Case insensitive regex to match "error" before variations of "borrow" including newlines
+    var regex = /\berror\b(?=[\s\S]*\bborrow(?:s|ing|ed)?\b)/i;
+
     fetch_with_timeout("https://play.rust-lang.org/evaluate.json", {
         headers: {
             'Content-Type': "application/json",
@@ -44,13 +60,23 @@ function evaluateCode() {
     })
     .then(response => response.json())
     .then(response => {
-        if (response.result.trim() === '') {
-            console.log("empty");
+        if (regex.test(response.result)) {
+            // o = 0
+            let A_i = Math.min(1, time_step/E_k)
+            skill_probs.push(
+                ( L*( A_i*S + 1-A_i ) ) / ( 1-A_i + A_i*(L*S+(1-L)*(1-G)) )
+            )
         } else {
-            console.log(response.result);
+            // o = 1
+            skill_probs.push(
+                ( L*(1-S) ) / ( L*(1-S) + (1-L)*G )
+            )
         }
     })
     .catch(error => console.log("Error during compilation: " + error.message));
+
+    time_step++
+    console.log(skill_probs)
 }
 
 function updateTimer() {
@@ -64,8 +90,7 @@ function updateTimer() {
     seconds--
     if (seconds === 0 && minutes === 0) {
         // timer goes to zero
-        clearInterval(evalID)
-        clearInterval(timerID)
+        done()
         alert("Time's up!")
     }
     else if (seconds < 0) {
@@ -83,14 +108,17 @@ function start() {
 
     let timerElement = document.getElementById('timer');
     timerElement.style.display = 'block'; // Show the timer element
-    timerElement.textContent = "00:10"
+    timerElement.textContent = START_TIME_LEN
 
-    evalID = setInterval(evaluateCode, time_step);
-    timerID = setInterval(updateTimer, 1000);
+    evalID = setInterval(evaluateCode, TIME_STEP_LEN);
+    timerID = setInterval(updateTimer, MILLI_PER_SEC);
+
+    sessionStorage.removeItem("td_bkt");
 }
 
 function done() {
     console.log("Stop code evaluation.")
-    clearInterval(evalID);
+    sessionStorage.setItem("td_bkt", JSON.stringify(skill_probs));
+    clearInterval(evalID)
     clearInterval(timerID)
 }
--- rs-tutor/src/problem.md
+++ rs-tutor/src/problem.md
@@ -2,13 +2,13 @@
 This problem is for tracking your borrowing skills. Write code to solve the problem. As you're writing code, your skills are tracked.
 
 ## Prompt
-Given a list of integers, implement a function `find_max_min_diff` that returns the difference between the maximum and minimum values in the list. You are not allowed to sort the list or use any built-in sorting functions.
+Given a list of integers, implement a function `find_max_min_diff` that returns the difference between the maximum and minimum values in the list. You are not allowed to sort the list or use any built-in sorting/max/min functions.
 
 Constraints:
 - The list `nums` will not be empty and will contain at least two elements.
 - The elements in `nums` will fit within the range of 32-bit signed integers (i32).
 
-Press the `Start` button to begin knowledge tracing. Once you're done, press `Done` to see the borrowing skill graph.
+Press the `Start` button to begin knowledge tracing. You have 15 minutes to complete the problem. A time will be displayed below when tracing begins. If you're done before 15 minutes, press `Done` to see the borrowing skill graph on the next page.
 <script src="js/problem.js"></script>
 <button onclick="start()">Start</button>
 ## Your Solution
@@ -18,9 +18,8 @@ fn find_max_min_diff(nums: &[i32]) -> i32 {
 }
 
 fn main() {
-    // Uncomment to test your code
-    // assert_eq!(find_max_min_diff(&[4, 1, 9, 3, 5]), 8);
-    // assert_eq!(find_max_min_diff(&[2, 8, 7, 1, 5, 3]), 7);
+    assert_eq!(find_max_min_diff(&[4, 1, 9, 3, 5]), 8);
+    assert_eq!(find_max_min_diff(&[2, 8, 7, 1, 5, 3]), 7);
 }
 ```
 <button onclick="done()">Done</button>
