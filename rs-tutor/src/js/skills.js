2662bd5c9c7409edadb4500c8f9777af0416a79d
Areeb Ahmed <5704619+Areeb196@users.noreply.github.com>
2024-07-17 20:27:40 -0500

    Sort of working

--- rs-tutor/src/js/skills.js
+++ rs-tutor/src/js/skills.js
@@ -1,4 +1,58 @@
+// constants
+N = 10
+
 // https://developers.google.com/chart/interactive/docs/gallery/linechart
 google.charts.load('current', {
     packages: ['corechart', 'line']
 });
+
+if (sessionStorage.getItem("td_bkt") === null) {
+    document.getElementById('chart_div').textContent = "No skill data saved ... visit the problem page to track your borrowing skill."
+}
+else {
+    google.charts.setOnLoadCallback(drawSkill);
+}
+
+function drawSkill() {
+    var data = new google.visualization.DataTable();
+    data.addColumn('number', 'Time Step');
+    data.addColumn('number', 'Probability');
+
+    let skillDataPoints = averageSkills(JSON.parse(sessionStorage.getItem("td_bkt")))
+
+    var rows = skillDataPoints.map(function(value, index) {
+        return [index + 1, value];
+    });
+    data.addRows(rows);
+
+    var options = {
+        hAxis: {
+          title: 'Time Step'
+        },
+        vAxis: {
+          title: 'Having Skill Probability',
+          minValue: 0,
+          maxValue: 1
+        }
+      };
+
+      var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+      chart.draw(data, options);
+}
+
+function averageSkills(skills) {
+    let res = [];
+    for (let i = skills.length-1; i >= 0; i--) {
+        let left = Math.max(i-N+2, 0)
+        var sliceArray = skills.slice(left, i);
+        var sum = sliceArray.reduce(function(total, currentValue) {
+            return total + currentValue;
+        }, 0);
+        
+        // Calculate the average
+        var average = sum / sliceArray.length;
+        res.push(average);
+    }
+    console.log(res, skills)
+    return res.reverse();
+}
--- rs-tutor/src/td-bkt.md
+++ rs-tutor/src/td-bkt.md
@@ -1,4 +1,4 @@
 # Skill Graph
+<div id="chart_div" style="width: 100%; height: 500px; margin: 0 auto;"></div>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script src="js/skills.js"></script>
-<div id="chart_div"></div>
